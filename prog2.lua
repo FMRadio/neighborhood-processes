226388ced5c286333970ce2089165aaf5d47c987
FMRadio <sporty_chick2013@hotmail.com>
2017-02-11 11:23:50 -0700

    Added the functions from Weiss we can use and added Katie to repo

--- prog2.lua
+++ prog2.lua
@@ -2,7 +2,7 @@
   * * * * prog2.lua * * * *
 Digital Image Process Program #2: This program does a multitude of neighborhood processes to a given image. 
 
-Author: Forrest Miller
+Authors: Forrest Miller and Katie MacMillan
 Class: CSC442/542 Digital Image Processing
 Date: 3/14/2017
 --]]
@@ -24,50 +24,40 @@ local threshold = require "il.threshold"
 imageMenu("Point Processes",
   {
     {"Grayscale", il.grayscaleYIQ},
-    
+    {"Binary Threshold", il.threshold,
+      {{name = "threshold", type = "number", displaytype = "slider", default = 128, min = 0, max = 255}}},
   })
 
--- neighborhood processes menu
-imageMenu("Neighborhood Processes",
-  {
-    --{"Negate", myip.negate,
-      --{{name = "Color Mode", type = "string", default = "rgb"}}},
-    --{"Brighten/Darken", myip.brightDark,
-      --{{name = "Offset", type = "number", displaytype = "slider", default = 0, min = -255, max = 255},
-      --{name = "Color Mode", type = "string", default = "rgb"}}},
-    --{"Adjust Contrast", myip.contrast,
-        --{{name = "min", type = "number", displaytype = "spin", default = 64, min = 0, max = 255},
-        --{name = "max", type = "number", displaytype = "spin", default = 191, min = 0, max = 255}}},
-    --{"Grayscale", myip.grayscale},
-    --{"Solarize", myip.solarize,
-        --{{name = "Threshold", type = "number", displaytype = "slider", default = 128, min = 0, max = 255}}},
-    --{"Binary Threshold", myip.binary,
-      --{{name = "Threshold", type = "number", displaytype = "slider", default = 128, min = 0, max = 255}}},
-    --{"Bit Plane Slicing", myip.bitPlane,
-      --{{name = "Bit (0 is LSB)", type = "number", displaytype = "spin", default = 0, min = 0, max = 7}}},
-    --{"Gamma", myip.gamma, {{name = "Gamma", type = "number", displaytype = "textbox", default = "1.0"}}},
-    })
-  
 -- histogram processes
 imageMenu("Histogram Processes",
   {
     {"Display Histogram", il.showHistogram,
       {{name = "Color Mode", type = "string", default = "rgb"}}},
-    --{"Equalize - Specify", myHist.equalize,
-      --{{name = "Clip %", type = "number", displaytype = "textbox", default = "1.0"}}},
-    --{"Equalize - Auto", myHist.equalize},
-    --{"Contrast Stretch - Specify", myHist.contrastStretch,
-      --{{name = "Min Percent", type = "number", displaytype = "slider", default = 1, min = 0, max = 50},
-      --{name = "Max Percent", type = "number", displaytype = "slider", default = 1, min = 0, max = 50}}},
-    --{"Contrast Stretch - Auto", myHist.contrastStretch},
+    {"Contrast Stretch", il.stretch,
+       {{name = "color model", type = "string", default = "yiq"}}},
+     {"Histogram Equalize", il.equalize,
+      {{name = "color model", type = "string", default = "yiq"}}},
   })
 
+imageMenu("Noise",
+  {
+    {"Gaussian noise", il.gaussianNoise,
+      {{name = "sigma", type = "number", displaytype = "textbox", default = "16.0"}}},
+    {"Impulse Noise", il.impulseNoise,
+      {{name = "probability", type = "number", displaytype = "slider", default = 64, min = 0, max = 1000}}},
+  })
+
+-- neighborhood processes menu
+imageMenu("Neighborhood Processes",
+  {
+    
+  })
 
 -- help menu
 imageMenu("Help",
   {
     {"Help", viz.imageMessage("Help", "Really good help.")},
-    {"About", viz.imageMessage("Lua Image Neighborhood Processing" .. viz.VERSION, "Authors: Forrest Miller\nClass: CSC442 Digital Image Processing\nDate: March 14th, 2017")},
+    {"About", viz.imageMessage("Lua Image Neighborhood Processing" .. viz.VERSION, "Authors: Forrest Miller and Katie MacMillan\nClass: CSC442 Digital Image Processing\nDate: March 14th, 2017")},
   }
 )
 
